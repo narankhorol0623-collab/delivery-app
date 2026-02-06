1c7b82d71ba1f67db402b965d9fbe6a1710b9853
25LP1579 25LP1579 <25LP1579@G304-CC0223-2.local>
2026-02-06 16:34:53 +0800

    test

--- server/src/utils/mail-utils.ts
+++ server/src/utils/mail-utils.ts
@@ -1,33 +1,24 @@
 import nodemailer from "nodemailer";
 import { configDotenv } from "dotenv";
+import { Resend } from "resend";
 
 configDotenv();
 
 const { APP_USER_MAIL, APP_PASSWORD } = process.env;
 
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: APP_USER_MAIL,
-    pass: APP_PASSWORD,
-  },
-});
+const resend = new Resend(process.env.RESET_API);
 
 export const verifyUserEmail = async (receiver: string, verifyLink: string) => {
-  try {
-    await transport.sendMail({
-      from: `"Food Delivery" <${APP_USER_MAIL}>`,
-      to: receiver,
-      subject: "Verify user link.",
-      html: `
+  await resend.emails.send({
+    from: `"Food Delivery" <${APP_USER_MAIL}>`,
+    to: receiver,
+    subject: "Verify user link.",
+    html: `
         <div style="width: 300px; padding: 20px; border-radius: 20px; background-color: blanchedalmond; text-align: center;">
           <a href="${verifyLink}" target="_blank" style="font-weight: bold; font-size: 20px; color: #ec4899; text-decoration: none; 
       }">
             Verify your Email!
           </a>
         </div>`,
-    });
-  } catch (error) {
-    console.error("Email send failed:", error);
-  }
+  });
 };
